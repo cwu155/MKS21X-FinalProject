e6aa39204fda642eb58b0282bc6497d3518d54f8
Joyce Wang <jwang01@stuy.edu>
2019-01-03 21:51:12 -0500

    Create subtract function that returns the difference of two substrings.

--- RealNumbers.java
+++ RealNumbers.java
@@ -3,4 +3,7 @@ public class RealNumbers{
   public int add(String one, String two){
     return Integer.parseInt(one) + Integer.parseInt(two);
   }
+  public int subtract(String one, String two){
+    return Integer.parseInt(one) - Integer.parseInt(two);
+  }
 }
