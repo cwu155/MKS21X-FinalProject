2536dc897e0bcae78a82d12f384d4cbb67637a18
cwu155 <cwu17@stuy.edu>
2019-01-06 21:45:25 -0500

    Did something to avoid merge conflict?

--- RealNumbers.java
+++ RealNumbers.java
@@ -2,28 +2,32 @@
 import java.util.*;
 
 public class RealNumbers{
+  String sub;
   //Given two substrings, return the sum
-  public static int add(String one, String two){
-    return Integer.parseInt(one) + Integer.parseInt(two);
+  public RealNumbers(String s){
+    sub = s;
+  }
+  public int add(String other){
+    return Integer.parseInt(this) + Integer.parseInt(other);
   }
   //Given two substrings, return the difference
-  public static int subtract(String one, String two){
-    return Integer.parseInt(one) - Integer.parseInt(two);
+  public int subtract(String other){
+    return Integer.parseInt(this) - Integer.parseInt(other);
   }
   //Given two substrings, return the product
-  public static int multiply(String one, String two){
-    return Integer.parseInt(one) * Integer.parseInt(two);
+  public int multiply(String other){
+    return Integer.parseInt(this) * Integer.parseInt(other);
   }
   //Given two substrings, return the quotient
-  public static int divide(String one, String two){
-    return Integer.parseInt(one) / Integer.parseInt(two);
+  public int divide(String other){
+    return Integer.parseInt(this) / Integer.parseInt(other);
   }
   //Given two substrings, return the GCF using Euclid's method
   //Made public for testing purposes
-  public static int GCF(String one, String two){
+  public int GCF(String other){
     //Variables for convenience
-    int num1 = Integer.parseInt(one);
-    int num2 = Integer.parseInt(two);
+    int num1 = Integer.parseInt(this);
+    int num2 = Integer.parseInt(other);
     //If the second number is 0, then the GCF is the first number
     if(num2 == 0){
       return num1;
@@ -32,12 +36,12 @@ public class RealNumbers{
     //num2 in num1's place and num2 replaced by the remainder of num1 and num2
     else{
       int temp = num1 % num2;
-      return GCF(two,temp + "");
+      return GCF(other,temp + "");
     }
   }
 
   // Given a value, return its square root using Newton's Method
-  public static double sqrt(String value){
+  public double sqrt(String value){
     double result = 1;
     double a = (Double.parseDouble(value));
       for (int i = 0; i < Double.parseDouble(value); i++){
@@ -47,17 +51,17 @@ public class RealNumbers{
     }
 
   //Testing methods
-  public static void main(String[] args) {
+  public void main(String[] args) {
     System.out.println("Testing RealNumbers");
 
-    String a = "4";
-    String b = "2";
-    System.out.println(add(a, b)); //6
-    System.out.println(subtract(a, b)); //2
-    System.out.println(multiply(a, b)); //8
-    System.out.println(divide(a, b)); //2
-    System.out.println(GCF("9","15")); //3
-    System.out.println(GCF("100","1")); //1
+    RealNumbers a = "4";
+    RealNumbers b = "2";
+    System.out.println(a.add(b)); //6
+    System.out.println(a.subtract(b)); //2
+    System.out.println(a.multiply(b)); //8
+    System.out.println(a.divide(b)); //2
+    System.out.println("9".GCF("15")); //3
+    System.out.println("100".GCF("1")); //1
     System.out.println(sqrt("4")); //2
   }
 }
