25d3a534309a8d8ad848498cfd5d8675f71e7e05
cwu155 <cwu17@stuy.edu>
2019-01-10 14:11:29 -0500

    Updated Equation class (Still unfinished)

--- Equation.java
+++ Equation.java
@@ -4,16 +4,20 @@ public class Equation{
   //Solving an expression or equation using PEMDAS - Order of Operations
   //Shunting-Yard Algorithm
 
+  public static String parenthesis = "()";
   public static String priority2 = "+-";
   public static String priority3 = "*/";
   public static String priority4 = "^";
+  public static Token top;
+  public static Integer result;
 
   //Creates an array of tokens.
-  public static String input = "4 + 18 / ( 9 - 3 )"; // Should take a user input in the future
-  public static StringTokenizer st = new StringTokenizer(input); //Create StringTokenizer Object
+  public static String infix; // Should take a user input in the future
+  public static StringTokenizer st; //Create StringTokenizer Object
   public static List<Token> Tokens = new ArrayList<Token>();   //Create ArrayList Object
   public static List<Token> Stack = new ArrayList<Token>(); //Creates a stack of operations.
   public static List<Token> Queue = new ArrayList<Token>(); //Creates a queue of the output.
+  public static List<Token> Numbers = new ArrayList<Token>();
 
   //Adds tokens from the user's input into ArrayList Tokens.
   public static void addTokens(){
@@ -48,62 +52,94 @@ public class Equation{
       return true;
   }
 
-        // While there are tokens to be read:
-        // Read a token
-        //   If it's a number add it to queue
-        //   If it's an operator
-        //         While there's an operator on the top of the stack with greater precedence:
-        //                 Pop operators from the stack onto the output queue
-        //                 Push the current operator onto the stack
-        //   If it's a left bracket push it onto the stack
-        //   If it's a right bracket
-        //       While there's not a left bracket at the top of the stack:
-        //               Pop operators from the stack onto the output queue.
-        //               Pop the left bracket from the stack and discard it
-        //       While there are operators on the stack, pop them to the queue
+  //Returns the element at the top of the stack.
+  public static Token peek(){
+    return Stack.get(Stack.size()-1);
+  }
 
-//Forms the operations stack and output stack. [Currently in progress]
-// 4 + 18 / (9 - 3) --> 4, 18, 9, 3, -, /, +
-// [4 + 18 / ( 9 - 3 )] size: 9
+  //Basically a pop function? Helper function!
+  public static void pop(){
+    String right = Numbers.get(Numbers.size()-1).getString();
+    String left = Numbers.get(Numbers.size()-2).getString();
+    String operator = peek().getString();
+    if (operator.equals("+")){result = Integer.valueOf(left) + Integer.valueOf(right);} else
+    if (operator.equals("-")){result = Integer.valueOf(left) - Integer.valueOf(right);} else
+    if (operator.equals("*")){result = Integer.valueOf(left) * Integer.valueOf(right);} else
+    if (operator.equals("/")){result = Integer.valueOf(left) / Integer.valueOf(right);} else
+    if (operator.equals("^")){result = Integer.valueOf(left) ^ Integer.valueOf(right);} else {
+      System.out.println(operator);
+    }
+    Token resultToken = new Token(Integer.toString(result), 1);
+    Numbers.add(resultToken);
+  }
 
-public static void formStacks(){
+
+public static void splitInfix(){
   for (int i = 0; i < Tokens.size(); i++){
     if (isNumber(Tokens.get(i)) == true){
       Queue.add(Tokens.get(i));
-     } else {
-       //if (Stack.size() == 0) {Stack.add(Tokens.get(i));} else {
-       //if (Stack.get(stackIter).getPriority() < Tokens.get(i).getPriority()){
-         Stack.add(Tokens.get(i));
-         //stackIter += 1;
-       }
-    //  }
-    if (Tokens.get(i).getString() == "("){
-      Stack.add(Tokens.get(i));
+      Numbers.add(Tokens.get(i));
     }
-    // if (Tokens.get(i).getString() == ")"){
-    //   while (Stack.get(Stack.size()-1).getString() != "("){
-    //     Queue.remove(Queue.indexOf("("));
+  }
+  for (int i = 0; i < Tokens.size(); i++){
+    if (isNumber(Tokens.get(i)) == false){
+      Queue.add(Tokens.get(i));
       }
-      }
-  // }
-  //}
-
-  public static void organizeStacks(){
-    for (Token element : Stack){
-      Queue.add(element);
     }
   }
 
+  public static String evaluate(String expression){
+    infix = expression;
+    st = new StringTokenizer(infix);
+    addTokens();
+    setPriority();
+    splitInfix();
+
+    for (Token element : Queue){
+      if (isNumber(element)){Numbers.add(element);
+      } else if (element.getString().equals("(")) {Stack.add(element);
+      } else if (element.getString().equals(")")){
+       top = peek();
+       while ((!(top.getString().equals(")")) && (top != null))){
+             pop();
+             top = peek();
+             for (Token thing : Stack){if (thing.getString().equals("(")){Stack.remove(thing);}}
+       }
+     } else {
+       if (Stack.size() == 0){Stack.add(element);} else {
+         top = peek();
+         while ((!(parenthesis.contains(element.getString()))) && (top.getPriority() > element.getPriority())){
+           pop();
+           top = peek();
+           Stack.add(element);
+         }
+       }
+     }
+    }
+
+    while (peek() != null) {pop();}
+    return Numbers.get(0).getString();
+  }
+
     public static void main(String[] args) { //Testing purposes
-      addTokens();
-      setPriority();
-      formStacks();
+      evaluate("4 + 18 / ( 9 - 3 )");
+
+      System.out.println("Current Queue");
       for(Token element : Queue) {
             System.out.println(element.getString());
         }
+
+      System.out.println("Current Stack");
       for(Token element : Stack){
+         System.out.println(element.getString());
+       }
+
+      System.out.println("Current Numbers");
+      for (Token element : Numbers){
         System.out.println(element.getString());
       }
+
+      System.out.println(peek().getString());
 
       }
     }
