d9a1f5cc0ee6af06ab32fc1e0eb03ed1c84f5cc4
joycevw02 <jwang01@stuy.edu>
2019-01-15 14:55:04 -0500

    Started reverse function

--- Equation.java
+++ Equation.java
@@ -3,28 +3,32 @@
 //Output should be something ike x = 1
 import java.util.*;
 public class Equation{
-  public static String ans = "";
+  public static String answer = "";
   public static String eval = "";
   public static void seperate(String s){
     s = s.trim();
     Boolean equal = false;
     for (int i = 0; i < s.length(); i ++){
       if (s.substring(i,i + 1).equals("=")){
-        System.out.println("HIT EQUAL!");
+//        System.out.println("HIT EQUAL!");
         equal = true;
         i ++;
       }
       if (!equal){
         eval += s.substring(i,i+1);
-        System.out.println("Eval " + eval);
+//        System.out.println("Eval " + eval);
       }
       else{
-        ans += s.substring(i,i+1);
-        System.out.println("Ans " + ans);
+        answer += s.substring(i,i+1);
+//        System.out.println("Answer " + answer);
       }
     }
-    System.out.println("Final Eval " + eval);
-    System.out.println("Final ans " + ans);
+//    System.out.println("Final Eval " + eval);
+//    System.out.println("Final answer " + answer);
+}
+  public static String reverse(String s){
+    String ans = "";
+    return ans;
   }
   public static void main(String[] args){
     seperate("4x + 1 = 5");
